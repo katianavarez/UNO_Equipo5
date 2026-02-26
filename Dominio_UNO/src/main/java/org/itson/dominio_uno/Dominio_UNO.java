c54dc82fa47583486d044c7b20d977d727b07b2c
Katia Návarez <katia.navarez252855@potros.itson.edu.mx>
2026-02-26 12:02:00 -0700

    Add files via upload

---
+++ Dominio_UNO/src/main/java/org/itson/dominio_uno/Dominio_UNO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.dominio_uno;
+
+/**
+ *
+ * @author katia
+ */
+public class Dominio_UNO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
